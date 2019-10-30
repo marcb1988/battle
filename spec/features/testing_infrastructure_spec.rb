2b1688511d9ea71a24ebf13bc3563e4b344c66e7
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-30 14:14:50 +0000

    refactored code to use sessions and post redirect

--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,9 +0,0 @@
-require './app.rb'
-
-
-xfeature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
